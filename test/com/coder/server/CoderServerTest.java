181d4b1f0079fac676c0550cc1945071da0b9f68
dcollin <dcollin@users.noreply.github.com>
2015-09-30 18:05:28 +0200

    Moved test code to a separate source folder
    And adde two dummy test cases for reference

---
+++ test/com/coder/server/CoderServerTest.java
@@ -0,0 +1,15 @@
+package com.coder.server;
+
+import junit.framework.TestCase;
+
+public class CoderServerTest extends TestCase {
+	
+	protected void setUp(){
+		
+	}
+	
+	public void testSomething(){
+		assertTrue(true);
+	}
+	
+}
